e4961dbdb9267861c80880f20ac828e67860ac7a
Shteyn <tarna83@mail.ru>
2019-04-05 10:05:02 +0200

    admin delete/approve functionality

--- student_portal_client/src/components/forms/ConfirmedUsersForm.js
+++ student_portal_client/src/components/forms/ConfirmedUsersForm.js
@@ -1,85 +1,79 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import {connect} from "react-redux";
-import {Table, Button} from "semantic-ui-react";
+import { connect } from "react-redux";
+import { Table, Button } from "semantic-ui-react";
 
-import {deleteUser} from "../../actions/user";
+import { deleteUser } from "../../actions/user";
 
 class ConfirmedUsersForm extends Component {
+  deleteUserHandler = id => {
+    console.log("deleteUser user CALLED", id);
+    this.props.deleteUser(id);
+  };
 
-    
+  render() {
+    const { allUsers } = this.props.allUsers;
 
-    deleteUserHandler = id => {
-        console.log("deleteUser user CALLED", id);
-        this.props.deleteUser(id);
-    };
+    return (
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Location</Table.HeaderCell>
+            <Table.HeaderCell>Class</Table.HeaderCell>
+            <Table.HeaderCell>First Name</Table.HeaderCell>
+            <Table.HeaderCell>Last Name</Table.HeaderCell>
+            <Table.HeaderCell>E-Mail</Table.HeaderCell>
+            <Table.HeaderCell />
+          </Table.Row>
+        </Table.Header>
 
-    render() {
-        const {allUsers} = this.props.allUsers;
-
-        return (
-            <Table celled>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>Location</Table.HeaderCell>
-                        <Table.HeaderCell>Class</Table.HeaderCell>
-                        <Table.HeaderCell>First Name</Table.HeaderCell>
-                        <Table.HeaderCell>Last Name</Table.HeaderCell>
-                        <Table.HeaderCell>E-Mail</Table.HeaderCell>
-                        <Table.HeaderCell/>
-                    </Table.Row>
-                </Table.Header>
-
-                <Table.Body>
-                    {allUsers.map(
-                        (oneUser) => {
-                            if (oneUser.confirmed) {
-                                return (
-                                    <Table.Row key={oneUser._id}>
-                                        <Table.Cell>{oneUser.location}</Table.Cell>
-                                        <Table.Cell>{oneUser.studentClass}</Table.Cell>
-                                        <Table.Cell>{oneUser.firstName}</Table.Cell>
-                                        <Table.Cell>{oneUser.lastName}</Table.Cell>
-                                        <Table.Cell>{oneUser.email}</Table.Cell>
-                                        <Table.Cell>
-                                            <Button
-                                                color="red"
-                                                onClick={this.deleteUserHandler.bind(this, oneUser._id)}
-                                            >
-                                                Delete User
-                                            </Button>
-                                        </Table.Cell>
-                                    </Table.Row>
-                                );
-                            } else {
-                                return null;
-                            }
-                        }
-                    )}
-                </Table.Body>
-            </Table>
-        );
-    }
+        <Table.Body>
+          {allUsers.map(oneUser => {
+            if (oneUser.confirmed) {
+              return (
+                <Table.Row key={oneUser._id}>
+                  <Table.Cell>{oneUser.location}</Table.Cell>
+                  <Table.Cell>{oneUser.studentClass}</Table.Cell>
+                  <Table.Cell>{oneUser.firstName}</Table.Cell>
+                  <Table.Cell>{oneUser.lastName}</Table.Cell>
+                  <Table.Cell>{oneUser.email}</Table.Cell>
+                  <Table.Cell>
+                    <Button
+                      color="red"
+                      onClick={this.deleteUserHandler.bind(this, oneUser._id)}
+                    >
+                      Delete User
+                    </Button>
+                  </Table.Cell>
+                </Table.Row>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </Table.Body>
+      </Table>
+    );
+  }
 }
 
 ConfirmedUsersForm.propTypes = {
-    allUsers: PropTypes.object.isRequired,
-    deleteUser: PropTypes.func.isRequired
+  allUsers: PropTypes.object.isRequired,
+  deleteUser: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state) {
-    console.log("CONFIRMED USERS", state.allUsers);
+  console.log("CONFIRMED USERS", state.allUsers);
 
-    return {
-        allUsers: state.allUsers
-    };
+  return {
+    allUsers: state.allUsers
+  };
 }
 
 export default connect(
-    mapStateToProps,
-    {deleteUser}
+  mapStateToProps,
+  { deleteUser }
 )(ConfirmedUsersForm);
-
 
 // {
 //   _id,
--- student_portal_client/src/components/pages/AdminDashboardPage.js
+++ student_portal_client/src/components/pages/AdminDashboardPage.js
@@ -1,69 +1,35 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { Tab } from "semantic-ui-react";
+import { Tabs, Tab } from "react-bootstrap";
 
 import WaitingUsersForm from "../forms/WaitingUsersForm";
 import ConfirmedUsersForm from "../forms/ConfirmedUsersForm";
 import { getAllUsers } from "../../actions/user";
 
-
-
-
-const panes = [
-  {
-    menuItem: "Waiting Users",
-    render: () => {
-      if(this.props && this.props.allUsers) {
-        const {allUsers} = this.props.allUsers;
-        console.log(allUsers)
-      }
-
-      return (
-          <Tab.Pane attached={false}>
-            <WaitingUsersForm/>
-          </Tab.Pane>
-      )
-    }
-  },
-  {
-    menuItem: "Confirmed Users",
-    render: () => (
-      <Tab.Pane attached={false}>
-        <ConfirmedUsersForm />
-      </Tab.Pane>
-    )
-  }
-];
-
 class AdminDashboardPage extends Component {
-
   componentDidMount() {
     this.props.getAllUsers();
   }
-
   render() {
-    const AdminTabs = () => <Tab menu={{ pointing: true }} panes={panes} />;
-
-    return <AdminTabs />;
+    return (
+      <Tabs defaultActiveKey="waitingStudents" id="uncontrolled-tab-example">
+        <Tab eventKey="waitingStudents" title="Waiting Students">
+          <WaitingUsersForm />
+        </Tab>
+        <Tab eventKey="confirmedStudents" title="Confirmed Students">
+          <ConfirmedUsersForm />
+        </Tab>
+      </Tabs>
+    );
   }
 }
 
 AdminDashboardPage.propTypes = {
-  allUsers: PropTypes.object.isRequired,
-  getAllUsers: PropTypes.func.isRequired,
-  isConfirmed: PropTypes.bool.isRequired
+  getAllUsers: PropTypes.func.isRequired
 };
 
-function mapStateToProps(state) {
-  console.log("ADMIN PAGE", state.allUsers);
-  return {
-    allUsers: state.allUsers,
-    isConfirmed: !!state.user.confirmed
-  };
-}
-
 export default connect(
-  mapStateToProps,
+  null,
   { getAllUsers }
 )(AdminDashboardPage);
