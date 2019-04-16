0aa7ca2b218ed6bc723a3d33bc984b0e2aec9d99
Shteyn <tarna83@mail.ru>
2019-04-16 18:29:59 +0200

    filter user by location - select location on registration

--- student_portal_client/src/components/pages/UserCard.js
+++ student_portal_client/src/components/pages/UserCard.js
@@ -3,27 +3,39 @@ import { Link, NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getAllUsers, filterLocation } from "../../actions/user";
-
 import TopNavigation from "../navigation/TopNavigation";
+import UserLocationFilter from "../forms/UserLocationFilter";
+import Search from "../forms/Search";
+
 class UserCard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      query: ""
+    };
+  }
+
   componentDidMount() {
     this.props.getAllUsers();
   }
 
-  filterLocation = e => {
-    console.log("event", e.target.value);
-    this.props.filterLocation(e.target.value);
+  searchChanged = query => {
+    this.setState({ query: query });
   };
+
+  /*filterLocation = e => {
+    console.log("filterLocation", e.target.value);
+    this.props.filterLocation(e.target.value);
+  };*/
   render() {
-    let { allUsers } = this.props.allUsers;
-    let { isConfirmedUser } = this.props.isConfirmed;
-    let placeholderUrl = require("../../img/placeholderUser.png");
+    let { isAuthenticated } = this.props;
 
     return (
       <div className="UserCardsCont">
         <div className="navigationBarUserCardPage">
-          {!isConfirmedUser && <TopNavigation />}
-          {isConfirmedUser && (
+          {isAuthenticated && <TopNavigation />}
+          {!isAuthenticated && (
             <div className="navigationBar">
               <Link to="/" className="label">
                 <span>DCI</span>
@@ -31,7 +43,7 @@ class UserCard extends Component {
               <ul>
                 <li>
                   <Link to="/login">
-                    <span>Sign In</span> | <span>Sign Up</span>
+                    <span>Login</span>
                   </Link>
                 </li>
               </ul>
@@ -43,70 +55,8 @@ class UserCard extends Component {
           <h1>OUR STUDENTS</h1>
           <h3>GRADUATED</h3>
           <h4>ALL LOCATIONS</h4>
-          <button onClick={this.filterLocation} value="Berlin">
-            Berlin
-          </button>
-
-          <div className="UserCardsItems">
-            {allUsers.map(oneUser => {
-              if (oneUser.confirmed) {
-                return (
-                  <div className="CardItem" key={oneUser._id}>
-                    {oneUser.userImage.length === 0 ? (
-                      <div
-                        className="profileImg"
-                        style={{
-                          backgroundImage: "url(" + placeholderUrl + ")"
-                        }}
-                      />
-                    ) : (
-                      <div
-                        className="profileImg"
-                        style={{
-                          backgroundImage:
-                            "url(" +
-                            `http://localhost:8080/uploads/${
-                              oneUser.userImage
-                            }` +
-                            ")"
-                        }}
-                      />
-                    )}
-
-                    <div className="userName">
-                      <p>{oneUser.firstName}</p>
-                      <p> {oneUser.lastName}</p>
-                    </div>
-
-                    <div className="locationAndAvailability">
-                      <div className="location">{oneUser.location}</div>
-                      <div className="Availability">
-                        <p>Availability</p>
-                        <p style={{ color: "white" }}>Available for offers</p>
-                      </div>
-                    </div>
-
-                    <div className="CardLinks">
-                      <NavLink title="Linked In" to="#">
-                        <img src={require("../../img/linkedin.png")} alt="" />
-                      </NavLink>
-                      <NavLink title="GitHub" to="#">
-                        <img src={require("../../img/github.png")} alt="" />
-                      </NavLink>
-                      <NavLink title="Xing" to="#">
-                        <img src={require("../../img/xing.png")} alt="" />
-                      </NavLink>
-                      <NavLink title="Portfolio" to="#">
-                        <img src={require("../../img/briefcase.png")} alt="" />
-                      </NavLink>
-                    </div>
-                  </div>
-                );
-              } else {
-                return null;
-              }
-            })}
-          </div>
+          <Search searchChanged={this.searchChanged} />
+          <UserLocationFilter query={this.state.query} />
         </div>
       </div>
     );
@@ -116,16 +66,13 @@ class UserCard extends Component {
 UserCard.propTypes = {
   allUsers: PropTypes.object.isRequired,
   getAllUsers: PropTypes.func.isRequired,
-  filterLocation: PropTypes.func.isRequired,
-  isConfirmed: PropTypes.bool.isRequired
+  isAuthenticated: PropTypes.bool.isRequired
 };
 
 function mapStateToProps(state) {
-  //console.log("state", state.allUsers);
-
   return {
     allUsers: state.allUsers,
-    isConfirmed: !!state.user.confirmed
+    isAuthenticated: !!state.user.token
   };
 }
 
--- student_portal_client/src/types.js
+++ student_portal_client/src/types.js
@@ -3,3 +3,4 @@ export const USER_LOGGED_OUT = "USER_LOGGED_OUT";
 export const NEW_USER = "NEW_USER";
 export const GET_ALL_USERS = "GET_ALL_USERS";
 export const GET_ONE_USER = "GET_ONE_USER";
+export const FILTER_USERS_BY_LOCATION = "FILTER_USERS_BY_LOCATION";
