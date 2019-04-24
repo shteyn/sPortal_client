af2d2992aaf19d3e504dedf3a03cb60d7fad817e
Viktor Zimmermann <viktorzimmermann11@web.de>
2019-04-24 14:33:19 +0200

    new version for HomePage and Footer

--- student_portal_client/src/components/pages/HomePage.js
+++ student_portal_client/src/components/pages/HomePage.js
@@ -8,36 +8,37 @@ import * as actions from "../../actions/auth";
 
 const HomePage = ({ isAuthenticated, logout }) => (
   <div>
-    {isAuthenticated && <TopNavigation />}
-    {!isAuthenticated && (
-      <div className="navigationBar">
-        <Link to="/" className="label">
-          <span>DCI</span>
-        </Link>
-        <ul>
-          <li>
-            <Link to="/login">
-              <span style={{ color: "black" }}>Login</span>
+    <div className="container-app">
+      {isAuthenticated && <TopNavigation />}
+      {!isAuthenticated && (
+          <div className="navigationBar">
+            <Link to="/" className="label">
+              <span>DCI</span>
             </Link>
-          </li>
-          {/*<li>*/}
-          {/*  <Link to="/registration">Registration</Link>*/}
-          {/*</li>*/}
-        </ul>
+            <ul>
+              <li>
+                <Link to="/login">
+                  <span>Login</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+      )}
+      <div className="centerWelcomeCont">
+        <div className="textCont">
+          <h1>WELCOME TO DCI.</h1>
+          <h1>SEE OUR STUDENTS</h1>
+        </div>
+        <button>
+          <Link to="/user-card">
+            <h3>See Our Students</h3>
+          </Link>
+        </button>
       </div>
-    )}
-    <div className="centerWelcomeCont">
-      <div className="textCont">
-        <h1>WELCOME TO DCI.</h1>
-        <h1>SEE OUR STUDENTS</h1>
-      </div>
-      <button>
-        <Link to="/user-card">
-          <h3>See Our Students</h3>
-        </Link>
-      </button>
     </div>
-    <Footer />
+    <div className="HomePageFooter">
+      <Footer />
+    </div>
   </div>
 );
 
